56d853b4150d2ee0aea15fcfb842facc976b5792
MrVictor42 <guin-hu@hotmail.com>
2019-07-31 17:11:21 -0300

    Deploy heroku

---
+++ build/precache-manifest.a25b5d3da1b4f3f0bddcaa0cfd821623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53829c864ebc590d05f38f2821eb28d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bea28b9c127e621b6f6",
+    "url": "/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "17a8840da134fede2730",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "2bea28b9c127e621b6f6",
+    "url": "/static/js/2.59ef6fe7.chunk.js"
+  },
+  {
+    "revision": "17a8840da134fede2730",
+    "url": "/static/js/main.3e4d96a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
