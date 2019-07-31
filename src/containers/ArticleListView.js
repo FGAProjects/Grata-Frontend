45340132e124eaf0e2be98e9e423ba87075c7fa2
MrVictor42 <guin-hu@hotmail.com>
2019-07-31 16:09:06 -0300

    Upload project to github

---
+++ src/containers/ArticleListView.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Articles from '../components/Article';
+import CustomForm from '../components/Form';
+import axios from 'axios';
+
+export default class ArticleList extends React.Component {
+
+    state = {
+        articles: []
+    }
+
+    componentDidMount() {
+        this.fetchDataFromApi();
+    }
+
+    fetchDataFromApi = () => {
+        axios.get('https://api-grata.herokuapp.com/api/')
+        .then(res => {
+            this.setState({
+                articles: res.data
+            });
+        });
+        setTimeout(() => {
+            this.fetchDataFromApi();
+        }, 100);
+    }
+
+    render() {
+        return (
+            <div>
+                <Articles data={this.state.articles}/>
+                <br />
+                <h2>Create an article</h2>
+                <CustomForm 
+                    requestType = 'post'
+                    articleID = {null}
+                    btnText = 'Create'
+                />
+            </div>
+        )
+    }
+}
